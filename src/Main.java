fd3157bf59e880adbf98ab64547a64a4161e38f5
MeongW <easymiracle@skuniv.ac.kr>
2024-04-01 01:21:00 +0900

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,29 @@
 import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int i=0; i<100; i++){
-            list.add(i+1);
-            System.out.println("Test123456");
+        ArrayList<Student> students = new ArrayList<Student>();
+        Student maxBmiStudent;
+
+        students.add(new Student("홍길동", 175, 75));
+        maxBmiStudent = students.getFirst();
+        students.add(new Student("이서경", 180, 78));
+        if (students.getLast().getBMI() > maxBmiStudent.getBMI()) maxBmiStudent = students.getLast();
+        students.add(new Student("이영희", 155, 49));
+        if (students.getLast().getBMI() > maxBmiStudent.getBMI()) maxBmiStudent = students.getLast();
+        students.add(new Student("김멋사", 182, 82));
+        if (students.getLast().getBMI() > maxBmiStudent.getBMI()) maxBmiStudent = students.getLast();
+
+        for (Student student: students) {
+            if (maxBmiStudent.equals(student)) {
+                students.remove(student);
+                break;
+            }
+        }
+
+        for (Student student: students) {
+            student.printIntro();
+            student.say();
         }
     }
 }
